d44addd9c7449e4c706ff964cd4d21b4ddf071bb
nearbaccus <41652937+nearbaccus@users.noreply.github.com>
2018-07-24 15:47:13 -0700

    Create icecream.js

---
+++ icecream.js
@@ -0,0 +1,25 @@
+function enlarge(word){
+  word.style.fontSize = "40px";
+}
+
+function smaller(word){
+  word.style.fontSize = "10px";
+}
+
+function enlargeTwo(cream){
+  cream.style.fontSize = "35px";
+  cream.style.color = "Blue";
+}
+
+function smallerTwo(cream){
+  cream.style.fontSize = "20px";
+  cream.style.color = "Purple";
+}
+
+function enlargeImage(image){
+  image.enlarge = width = "300px" height = "400px";
+}
+
+function smallerImage(image){
+  image.smaller = wid
+}
