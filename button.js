eb3d87ecda8f70920994a000a2c1748ff7d8c0e6
nearbaccus <41652937+nearbaccus@users.noreply.github.com>
2018-07-27 09:40:07 -0700

    Create button.js

---
+++ button.js
@@ -0,0 +1,23 @@
+
+function makeGrey(g){
+  document.getElementById(g).style.height = "75px";
+  document.getElementById(g).style.width = "125px";
+}
+
+function returnPurple(r){
+  document.getElementById(r).style.backgroundColor = "purple";
+  document.getElementById(r).style.width = "100px";
+  document.getElementById(r).style.height = "50px";
+}
+
+function returnGreen(e){
+  document.getElementById(e).style.backgroundColor = "green";
+  document.getElementById(e).style.height = "50px";
+  document.getElementById(e).style.width = "100px";
+}
+
+function returnOrange(o){
+  document.getElementById(o).style.backgroundColor = "orange";
+  document.getElementById(o).style.height = "50px";
+  document.getElementById(o).style.width = "100px";
+}
