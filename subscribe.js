fabd0cf1ef165980c722864eca893103145da8d2
nearbaccus <41652937+nearbaccus@users.noreply.github.com>
2019-03-17 16:40:12 -0700

    Add files via upload

---
+++ subscribe.js
@@ -0,0 +1,22 @@
+function testt()
+{
+var isFormValid = true;
+
+$(".sub input").each(function(){
+    if ($.trim($(this).val()).length < 1){
+        $(this).addClass("highlight");
+        isFormValid = false;
+    }
+    else{
+        $(this).removeClass("highlight");
+     }
+    });
+
+    if(isFormValid){
+       alert("Subscribed!");}
+
+    if (!isFormValid) {
+    alert("Please fill in all fields");
+}
+
+}
