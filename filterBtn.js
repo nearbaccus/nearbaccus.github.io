13269b3ff5475fb91bb0d71acfa22714b8381787
nearbaccus <41652937+nearbaccus@users.noreply.github.com>
2019-03-17 16:30:26 -0700

    Delete filterBtn.js

--- filterBtn.js
+++
@@ -1,8 +0,0 @@
-// Get the button, and when the user clicks on it, execute myFunction
-var num = 0;
-document.getElementById("filterBtn").onclick = function() {dropMenu()};
-
-/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
-function dropMenu() {
-  document.getElementById("dropdownd").classList.toggle("show");
-}
